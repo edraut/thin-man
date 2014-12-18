bcb8ec401c1e0a23a4d766790030784f1f3f732b
Eric Draut <edraut@gmail.com>
2014-12-18 10:04:34 -0700

    initial commit

---
+++ lib/tasks/thin_man_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :thin_man do
+#   # Task goes here
+# end
---
+++ test/thin_man_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ThinManTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, ThinMan
+  end
+end
