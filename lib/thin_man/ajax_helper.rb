a799476e82662336d822e73a3633077a1020c4c2
Eric Draut <edraut@gmail.com>
2015-02-27 11:09:09 -0700

    addedd empty-on-success option for helpers

--- lib/thin_man/ajax_helper.rb
+++ lib/thin_man/ajax_helper.rb
@@ -1,12 +1,13 @@
 module ThinMan
   module AjaxHelper
-    def ajax_link(name, options, html_options, target, sub_class: nil, insert_method: nil)
+    def ajax_link(name, options, html_options, target, sub_class: nil, insert_method: nil, empty_on_success: nil)
       ajax_options = {
         'data-ajax-link' => true,
         'data-ajax-target' => target
       }
       ajax_options.merge!('data-sub-type' => sub_class) if sub_class.present?
       ajax_options.merge!('data-insert-method' => insert_method) if insert_method.present?
+      ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
       link_to(name,
               options,
               html_options.merge(ajax_options))
@@ -23,20 +24,23 @@ module ThinMan
               html_options.merge(ajax_options))
     end
 
-    def ajax_form_hash(target, sub_class: nil, insert_method: 'html')
+    def ajax_form_hash(target, sub_class: nil, insert_method: nil, error_target: nil, empty_on_success: nil)
       ajax_options = {
         'data-ajax-form' => true,
-        'data-ajax-target' => target,
-        'data-insert-method' => insert_method
+        'data-ajax-target' => target
       }
+      ajax_options.merge!('data-insert-method' => insert_method) if insert_method.present?
       ajax_options.merge!('data-sub-type' => sub_class) if sub_class.present?
+      ajax_options.merge!('data-error-target' => error_target) if error_target.present?
+      ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
       ajax_options
     end
 
-    def ajax_link_attrs(target, insert_method: nil, sub_type: nil)
+    def ajax_link_attrs(target, insert_method: nil, sub_type: nil, empty_on_success: nil)
       data_attrs = "data-ajax-link=true data-ajax-target=#{target}"
       data_attrs += " data-insert-method=#{insert_method}" if insert_method
-      data_attrs += " data-insert-method=#{sub_type}" if sub_type
+      data_attrs += " data-sub-type=#{sub_type}" if sub_type
+      data_attrs += " data-empty-on-success=#{empty_on_success}" if empty_on_success
       data_attrs
     end
 
