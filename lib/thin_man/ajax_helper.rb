9039b8cb9f903a769fcd838109213e1d663bfd21
Eric Draut <edraut@gmail.com>
2017-08-02 17:52:16 -0600

    added remove_on_success option for links/forms

--- lib/thin_man/ajax_helper.rb
+++ lib/thin_man/ajax_helper.rb
@@ -1,7 +1,7 @@
 module ThinMan
   module AjaxHelper
     def ajax_link(name, options, html_options, target,
-      sub_class: nil, insert_method: nil, empty_on_success: nil,
+      sub_class: nil, insert_method: nil, empty_on_success: nil, remove_on_success: nil,
       http_method: nil, no_mouse_click: nil, progress_target: nil,
       mask_target: nil, mask_message: nil,
       progress_color: nil, scroll_to: nil)
@@ -13,6 +13,7 @@ module ThinMan
       ajax_options.merge!('data-insert-method' => insert_method) if insert_method.present?
       ajax_options.merge!('data-ajax-method' => http_method) if http_method.present?
       ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
+      ajax_options.merge!('data-remove-on-success' => remove_on_success) if remove_on_success.present?
       ajax_options.merge!('data-no-mouse-click' => no_mouse_click) if no_mouse_click.present?
       ajax_options.merge!('data-progress-target' => progress_target) if progress_target.present?
       ajax_options.merge!('data-progress-color' => progress_color) if progress_color.present?
@@ -25,7 +26,7 @@ module ThinMan
     end
 
     def ajax_link_now(name, options, html_options, target,
-      sub_class: nil, insert_method: nil, empty_on_success: nil,
+      sub_class: nil, insert_method: nil, empty_on_success: nil, remove_on_success: nil,
       http_method: nil, no_mouse_click: nil, progress_target: nil,
       progress_color: nil, mask_target: nil, mask_message: nil,
       sequence_group: nil, sequence_number: nil)
@@ -37,6 +38,7 @@ module ThinMan
       ajax_options.merge!('data-insert-method' => insert_method) if insert_method.present?
       ajax_options.merge!('data-ajax-method' => http_method) if http_method.present?
       ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
+      ajax_options.merge!('data-remove-on-success' => remove_on_success) if remove_on_success.present?
       ajax_options.merge!('data-no-mouse-click' => no_mouse_click) if no_mouse_click.present?
       ajax_options.merge!('data-progress-target' => progress_target) if progress_target.present?
       ajax_options.merge!('data-progress-color' => progress_color) if progress_color.present?
@@ -65,7 +67,7 @@ module ThinMan
     end
 
     def ajax_form_hash(target, sub_class: nil, insert_method: nil,
-      error_target: nil, empty_on_success: nil, reset_on_success: nil,
+      error_target: nil, empty_on_success: nil, reset_on_success: nil, remove_on_success: nil,
       container: nil, custom_progress: nil, no_mouse_click: nil,
       mask_target: nil, mask_message: nil,
       progress_target: nil, progress_color: nil)
@@ -78,6 +80,7 @@ module ThinMan
       ajax_options.merge!('data-container' => container) if container.present?
       ajax_options.merge!('data-error-target' => error_target) if error_target.present?
       ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
+      ajax_options.merge!('data-remove-on-success' => remove_on_success) if remove_on_success.present?
       ajax_options.merge!('data-reset-on-success' => reset_on_success) if reset_on_success.present?
       ajax_options.merge!('data-custom-progress' => custom_progress) if custom_progress.present?
       ajax_options.merge!('data-no-mouse-click' => no_mouse_click) if no_mouse_click.present?
@@ -90,7 +93,7 @@ module ThinMan
 
     def ajax_form_now(target, sub_class: nil,
       insert_method: nil, error_target: nil,
-      empty_on_success: nil, reset_on_success: nil,
+      empty_on_success: nil, reset_on_success: nil, remove_on_success: nil,
       container: nil, custom_progress: nil,
       mask_target: nil, mask_message: nil,
       progress_target: nil, progress_color: nil)
@@ -103,6 +106,7 @@ module ThinMan
       ajax_options.merge!('data-container' => container) if container.present?
       ajax_options.merge!('data-error-target' => error_target) if error_target.present?
       ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
+      ajax_options.merge!('data-remove-on-success' => remove_on_success) if remove_on_success.present?
       ajax_options.merge!('data-reset-on-success' => reset_on_success) if reset_on_success.present?
       ajax_options.merge!('data-custom-progress' => custom_progress) if custom_progress.present?
       ajax_options.merge!('data-progress-target' => progress_target) if progress_target.present?
@@ -113,7 +117,7 @@ module ThinMan
     end
 
     def ajax_form_attrs(target, sub_class: nil,
-      insert_method: nil, error_target: nil,
+      insert_method: nil, error_target: nil, remove_on_success: nil,
       empty_on_success: nil, reset_on_success: nil, no_mouse_click: nil,
       progress_target: nil, progress_color: nil,
       mask_target: nil, mask_message: nil )
@@ -121,6 +125,7 @@ module ThinMan
       data_attrs += " data-insert-method=#{insert_method}" if insert_method
       data_attrs += " data-sub-type=#{sub_class}" if sub_class
       data_attrs += " data-empty-on-success=#{empty_on_success}" if empty_on_success
+      data_attrs += " data-remove-on-success=#{remove_on_success}" if remove_on_success
       data_attrs += " data-reset-on-success=#{reset_on_success}" if reset_on_success
       data_attrs += " data-error-target=#{error_target}" if error_target
       data_attrs += " data-no-mouse-click=#{no_mouse_click}" if no_mouse_click
@@ -133,13 +138,14 @@ module ThinMan
 
     def ajax_form_now_attrs(target, sub_class: nil,
       insert_method: nil, error_target: nil,
-      empty_on_success: nil, reset_on_success: nil,
+      empty_on_success: nil, reset_on_success: nil, remove_on_success: nil,
       progress_target: nil, progress_color: nil,
       mask_target: nil, mask_message: nil )
       data_attrs = "data-ajax-form-now=true data-ajax-target=#{target}"
       data_attrs += " data-insert-method=#{insert_method}" if insert_method
       data_attrs += " data-sub-type=#{sub_class}" if sub_class
       data_attrs += " data-empty-on-success=#{empty_on_success}" if empty_on_success
+      data_attrs += " data-remove-on-success=#{remove_on_success}" if remove_on_success
       data_attrs += " data-reset-on-success=#{reset_on_success}" if reset_on_success
       data_attrs += " data-error-target=#{error_target}" if error_target
       data_attrs += " data-progress-target=#{progress_target}" if progress_target
@@ -150,7 +156,7 @@ module ThinMan
     end
 
     def ajax_link_attrs(target,
-      insert_method: nil, sub_type: nil, empty_on_success: nil,
+      insert_method: nil, sub_type: nil, empty_on_success: nil, remove_on_success: nil,
       http_method: nil, no_mouse_click: nil, progress_target: nil,
       progress_color: nil,
       mask_target: nil, mask_message: nil,
@@ -160,6 +166,7 @@ module ThinMan
       data_attrs += " data-ajax-method=#{http_method}" if http_method
       data_attrs += " data-sub-type=#{sub_type}" if sub_type
       data_attrs += " data-empty-on-success=#{empty_on_success}" if empty_on_success
+      data_attrs += " data-remove-on-success=#{remove_on_success}" if remove_on_success
       data_attrs += " data-no-mouse-click=#{no_mouse_click}" if no_mouse_click
       data_attrs += " data-progress-target=#{progress_target}" if progress_target
       data_attrs += " data-progress-color=#{progress_color}" if progress_color
