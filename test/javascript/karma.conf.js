cc7705b670ff6b77fd3e85bf219d855fa8a19045
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-11-16 13:40:29 -0700

    adds tests for three types of ajaxDelete scenarios

--- test/javascript/karma.conf.js
+++ test/javascript/karma.conf.js
@@ -2,70 +2,75 @@
 // Generated on Thu Sep 10 2015 16:43:39 GMT-0500 (COT)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine-ajax', 'jasmine', 'requirejs'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'test-main.js',
-      {pattern: 'jquery.js'},
-      {pattern: 'jasmine-fixture.js'},
-      {pattern: '../../app/assets/javascripts/thin_man.js'},
-      {pattern: '../../app/assets/javascripts/debug_logger.js'},
-      {pattern: 'spec/helpers/*.js'},
-      {pattern: 'spec/**/*Spec.js', included: false}
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine-ajax', 'jasmine', 'requirejs'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'test-main.js',
+            { pattern: 'jquery.js' },
+            { pattern: 'jasmine-fixture.js' },
+            { pattern: '../../app/assets/javascripts/thin_man.js' },
+            { pattern: '../../app/assets/javascripts/debug_logger.js' },
+            { pattern: 'spec/helpers/*.js' },
+            { pattern: 'spec/**/*Spec.js', included: false }
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['spec'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  })
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+        // customLaunchers: {
+        //     'PhantomJS_custom': {
+        //         base: 'PhantomJS',
+        //         debug: true,
+        //     },
+        // },
+        // browsers: ['Chrome'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    })
 }
