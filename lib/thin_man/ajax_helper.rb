7e75081bfbdd6acf43087fb8848c866fe19f95c1
Eric Draut <edraut@gmail.com>
2018-10-30 13:45:37 -0600

    push_path and replacement_path now work

--- lib/thin_man/ajax_helper.rb
+++ lib/thin_man/ajax_helper.rb
@@ -3,7 +3,7 @@ module ThinMan
     def ajax_link(name, options, html_options, target,
       sub_class: nil, insert_method: nil, empty_on_success: nil, remove_on_success: nil,
       http_method: nil, no_mouse_click: nil, progress_target: nil,
-      mask_target: nil, mask_message: nil, replacement_path: nil,
+      mask_target: nil, mask_message: nil, replacement_path: nil, push_path: nil,
       progress_color: nil, scroll_to: nil)
       ajax_options = {
         'data-ajax-link' => true,
@@ -21,6 +21,7 @@ module ThinMan
       ajax_options.merge!('data-mask-message' => mask_message) if mask_message.present?
       ajax_options.merge!('data-scroll-to' => scroll_to) if scroll_to.present?
       ajax_options.merge!('data-replacement-path' => replacement_path) if replacement_path.present?
+      ajax_options.merge!('data-push-path' => push_path) if push_path.present?
       link_to(name,
               options,
               html_options.merge(ajax_options))
@@ -30,7 +31,7 @@ module ThinMan
       sub_class: nil, insert_method: nil, empty_on_success: nil, remove_on_success: nil,
       http_method: nil, no_mouse_click: nil, progress_target: nil,
       progress_color: nil, mask_target: nil, mask_message: nil,
-      sequence_group: nil, sequence_number: nil, replacement_path: nil,
+      sequence_group: nil, sequence_number: nil, replacement_path: nil, push_path: nil,
       search_path: nil, search_params: nil)
       ajax_options = {
         'data-ajax-link-now' => true,
@@ -51,6 +52,7 @@ module ThinMan
       ajax_options.merge!('data-search-path' => search_path) if search_path.present?
       ajax_options.merge!('data-search-params' => search_params) if search_params.present?
       ajax_options.merge!('data-replacement-path' => replacement_path) if replacement_path.present?
+      ajax_options.merge!('data-push-path' => push_path) if push_path.present?
 
       a_tag = link_to(name,
                 options,
@@ -81,7 +83,7 @@ module ThinMan
     def ajax_form_hash(target, sub_class: nil, insert_method: nil,
       error_target: nil, empty_on_success: nil, reset_on_success: nil, remove_on_success: nil,
       container: nil, custom_progress: nil, no_mouse_click: nil,
-      mask_target: nil, mask_message: nil, scroll_to: nil, replacement_path: nil,
+      mask_target: nil, mask_message: nil, scroll_to: nil, replacement_path: nil, push_path: nil,
       progress_target: nil, progress_color: nil)
       ajax_options = {
         'data-ajax-form' => true,
@@ -102,6 +104,7 @@ module ThinMan
       ajax_options.merge!('data-mask-message' => mask_message) if mask_message.present?
       ajax_options.merge!('data-scroll-to' => scroll_to) if scroll_to.present?
       ajax_options.merge!('data-replacement-path' => replacement_path) if replacement_path.present?
+      ajax_options.merge!('data-push-path' => push_path) if push_path.present?
       ajax_options
     end
 
@@ -109,7 +112,7 @@ module ThinMan
       insert_method: nil, error_target: nil,
       empty_on_success: nil, reset_on_success: nil, remove_on_success: nil,
       container: nil, custom_progress: nil,
-      mask_target: nil, mask_message: nil, replacement_path: nil,
+      mask_target: nil, mask_message: nil, replacement_path: nil, push_path: nil,
       progress_target: nil, progress_color: nil)
       ajax_options = {
         'data-ajax-form-now' => true,
@@ -128,13 +131,14 @@ module ThinMan
       ajax_options.merge!('data-mask-target' => mask_target) if mask_target.present?
       ajax_options.merge!('data-mask-message' => mask_message) if mask_message.present?
       ajax_options.merge!('data-replacement-path' => replacement_path) if replacement_path.present?
+      ajax_options.merge!('data-push-path' => push_path) if push_path.present?
       ajax_options
     end
 
     def ajax_form_attrs(target, sub_class: nil,
       insert_method: nil, error_target: nil, remove_on_success: nil,
       empty_on_success: nil, reset_on_success: nil, no_mouse_click: nil,
-      progress_target: nil, progress_color: nil, scroll_to: nil, replacement_path: nil,
+      progress_target: nil, progress_color: nil, scroll_to: nil, replacement_path: nil, push_path: nil,
       mask_target: nil, mask_message: nil )
       data_attrs = "data-ajax-form=true data-ajax-target=#{target}"
       data_attrs += " data-insert-method=#{insert_method}" if insert_method
@@ -150,13 +154,14 @@ module ThinMan
       data_attrs += " data-mask-message=#{mask_message}" if mask_message
       data_attrs += " data-scroll-to=#{scroll_to}" if scroll_to
       data_attrs += " data-replacement-path=#{replacement_path}" if replacement_path
+      data_attrs += " data-push-path=#{push_path}" if push_path
       data_attrs
     end
 
     def ajax_form_now_attrs(target, sub_class: nil,
       insert_method: nil, error_target: nil,
       empty_on_success: nil, reset_on_success: nil, remove_on_success: nil,
-      progress_target: nil, progress_color: nil, replacement_path: nil,
+      progress_target: nil, progress_color: nil, replacement_path: nil, push_path: nil,
       mask_target: nil, mask_message: nil )
       data_attrs = "data-ajax-form-now=true data-ajax-target=#{target}"
       data_attrs += " data-insert-method=#{insert_method}" if insert_method
@@ -170,13 +175,14 @@ module ThinMan
       data_attrs += " data-mask-target=#{mask_target}" if mask_target
       data_attrs += " data-mask-message=#{mask_message}" if mask_message
       data_attrs += " data-replacement-path=#{replacement_path}" if replacement_path
+      data_attrs += " data-push-path=#{push_path}" if push_path
       data_attrs
     end
 
     def ajax_link_attrs(target,
       insert_method: nil, sub_type: nil, empty_on_success: nil, remove_on_success: nil,
       http_method: nil, no_mouse_click: nil, progress_target: nil,
-      progress_color: nil, replacement_path: nil,
+      progress_color: nil, replacement_path: nil, push_path: nil,
       mask_target: nil, mask_message: nil,
       scroll_to: nil)
       data_attrs = "data-ajax-link=true data-ajax-target=#{target}"
@@ -192,7 +198,33 @@ module ThinMan
       data_attrs += " data-mask-target=#{mask_target}" if mask_target
       data_attrs += " data-mask-message=#{mask_message}" if mask_message
       data_attrs += " data-replacement-path=#{replacement_path}" if replacement_path
+      data_attrs += " data-push-path=#{push_path}" if push_path
       data_attrs
+    end
+
+    def ajax_link_hash(target,
+      insert_method: nil, sub_type: nil, empty_on_success: nil, remove_on_success: nil,
+      http_method: nil, no_mouse_click: nil, progress_target: nil,
+      progress_color: nil, replacement_path: nil, push_path: nil,
+      mask_target: nil, mask_message: nil,
+      scroll_to: nil)
+      ajax_options = {
+        'data-ajax-link' => true,
+        'data-ajax-target' => target
+      }
+      ajax_options.merge!('data-insert-method' => insert_method) if insert_method.present?
+      ajax_options.merge!('data-ajax-method' => http_method) if http_method.present?
+      ajax_options.merge!('data-empty-on-success' => empty_on_success) if empty_on_success.present?
+      ajax_options.merge!('data-remove-on-success' => remove_on_success) if remove_on_success.present?
+      ajax_options.merge!('data-no-mouse-click' => no_mouse_click) if no_mouse_click.present?
+      ajax_options.merge!('data-progress-target' => progress_target) if progress_target.present?
+      ajax_options.merge!('data-progress-color' => progress_color) if progress_color.present?
+      ajax_options.merge!('data-mask-target' => mask_target) if mask_target.present?
+      ajax_options.merge!('data-mask-message' => mask_message) if mask_message.present?
+      ajax_options.merge!('data-scroll-to' => scroll_to) if scroll_to.present?
+      ajax_options.merge!('data-replacement-path' => replacement_path) if replacement_path.present?
+      ajax_options.merge!('data-push-path' => push_path) if push_path.present?
+      ajax_options
     end
 
     def dom_target(resource, label = nil)
